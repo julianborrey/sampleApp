6b185c83ce78722d58a47b9440e713e99ca5c0f1
julianborrey <julian.borrey@duke.edu>
2013-07-17 13:27:20 +1000

    finished ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "user/new"
    root 'static_page#home'
 
    match '/help',    to: 'static_page#help',    via: 'get'
    match '/about',   to: 'static_page#about',   via: 'get'
    match '/contact', to: 'static_page#contact', via: 'get'
+   match '/signup',  to: 'user#new',            via: 'get'
 end
