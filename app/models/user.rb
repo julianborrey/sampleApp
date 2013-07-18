4ae4536482fdb72a6998db910ef3a97da15544ec
julianborrey <julian.borrey@duke.edu>
2013-07-18 13:28:47 +1000

    finished ch6 + exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
    #ensure that we are going to input an all lowercase email (helps with uniqueness check)
-   before_save { self.email = email.downcase }
-
+   #before_save { self.email = email.downcase } #old version
+   before_save { email.downcase! }#new verions
+   
    validates :name,  presence: true, length: { maximum: 40 } #just a function validates(:name, presece: true);
    
    #make a REGex
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
                      #put uniqueness check on db as well to prevent double submit error
+   
+   has_secure_password #contains the validation against blank entries
+   validates :password, length: { minimum: 6 } 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe User do
    #before we run a certain function, in this case the constructor
-   before { @user = User.new(name: "Example User", email: "user@example.com") }
+   before { @user = User.new(name: "Example User", email: "user@example.com",
+                             password: "foobar", password_confirmation: "foobar") }
    
    #make the user object the subject of the following test
    subject { @user }
@@ -10,7 +11,11 @@ describe User do
    #do some testing. Just checks that these fields exist.
    it { should respond_to(:name)  }
    it { should respond_to(:email) }
-   
+   it { should respond_to(:password_digest) }
+   it { should respond_to(:password) }
+   it { should respond_to(:password_confirmation) }
+   it { should respond_to(:authenticate) }
+
    ###Note: we could have used the RSpec code to do the same tests
    #it "should have field 'name'" do
    #   expect(@user).to respond_to(:name)
@@ -66,6 +71,17 @@ describe User do
       end
    end
    
+   #ensure that the email is entered into the db with all lowercase
+   describe "email address with mixed case" do
+      let(:mixed_case_email) { "Foo@ExamPle.CoM" }
+
+      it "should be all saved as lower case" do
+         @user.email = mixed_case_email
+         @user.save
+         expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
+   end
+   
    #check test for ensuring the submitted email is unique
    describe "when email address is already taken" do
       before do
@@ -77,6 +93,46 @@ describe User do
       end                          #which should trigger the error
       
       it { should_not be_valid }
+   end
+   
+   ## Password Checks
+   #check for blank password
+   describe "when password is not present" do
+      before do
+         @user = User.new(name: "Example User", email: "user@example.com",
+                          password: " ", password_confirmation: " ")
+      end
+      it { should_not be_valid }
+   end
+   
+   #check for password length (min of 6 chars)
+   describe "with a password that's too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5 }
+      it { should be_invalid }
+   end
+   
+   #check for password mismatch
+   describe "when password doesn't match confirmation" do
+      before { @user.password_confirmation = "mismatch" }
+      it { should_not be_valid }
+   end
+
+   ### Authentication Checks ###
+   describe "return value of authenticate method" do
+      before { @user.save }
+      let(:found_user) { User.find_by(email: @user.email) }
+      
+      describe "with valid password" do
+         it { should eq found_user.authenticate(@user.password) }
+      end
+      
+      describe "with invalid password" do
+         #using some string ("invalid") which is not the password
+         let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+         
+         it { should_not eq user_for_invalid_password }
+         specify { expect(user_for_invalid_password).to be_false }
+      end
    end
 
 end
